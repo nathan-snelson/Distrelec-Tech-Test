28d3b8503a45135d1157616e871962f1c33db1b8
Nathan Snelson <nathansnelson97@gmail.com>
2021-07-10 11:39:12 +0100

    Initial setup

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    // directory name: 'build directory'
+    public: '/',
+    src: '/dist',	
   },
   plugins: [
-    /* ... */
+    ['@snowpack/plugin-react-refresh'],
+    ['@snowpack/plugin-sass',{
+      style: 'compressed'
+    }],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
