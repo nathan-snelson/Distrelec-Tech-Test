5688241939dcde444a10564fa82851e38175a568
Nathan Snelson <nathansnelson97@gmail.com>
2021-07-11 11:19:56 +0100

    Initial logic for Carousel added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import 'Carousel.css';
-import * as data from './src/data.json'
+import './Carousel.css'
 
-const Carousel = () => {
+const LeftNav = () => {
+    
+}
 
+const RightNav = () => {
+
+}
+
+const Carousel = ({data}) => {
+    const [current, setCurrent] = useState(0);
+    
+    const nextSlide = () => {
+        setCurrent(current === data.length - 1 ? 0 : current + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? data.length - 1 : current - 1);
+    }
+
+    return (        
+        <div>    
+            <div>
+            {
+                data.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            {index === current && <img src={item.productImageUrl} alt={item.productImageAltText}/>}
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
 }
 
 export default Carousel;
